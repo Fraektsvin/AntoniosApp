50917be7700d3c15edb742047dd2dfa68ae1a0ea
Antonio <32570073+Fraektsvin@users.noreply.github.com>
2021-06-14 00:02:45 +0200

    adasd
    fjernet kkey

--- DannyApp/app/src/main/java/com/example/dannyappPokemonApp/Util/Constants.java
+++ DannyApp/app/src/main/java/com/example/dannyappPokemonApp/Util/Constants.java
@@ -3,8 +3,8 @@ package com.example.dannyappPokemonApp.Util;
 public class Constants {
 
     public static final String Base_URL = "https://api.pokemontcg.io";
-            // public static final String API_KEY =  "6dc373b0-353f-4172-9bd6-67cca65865c2";
-    public static final String API_KEY= "89f9bb31-4511-4872-b933-e2ffdb895b38";
+            // public static final String API
+    public static final String API_KEY= "";
     public static final int CONNECTION_TIMEOUT = 3000;
     public static final int READ_TIMEOUT = 2;
     public static final int WRITE_TIMEOUT = 2;
