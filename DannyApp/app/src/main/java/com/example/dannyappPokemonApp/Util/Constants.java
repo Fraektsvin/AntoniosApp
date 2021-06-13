c293d6ffa097dc1766658cd7759c3184543ac7d1
Antonio <32570073+Fraektsvin@users.noreply.github.com>
2021-06-14 01:04:21 +0200

    ændring til ny
    gdd

--- DannyApp/app/src/main/java/com/example/dannyappPokemonApp/Util/Constants.java
+++ DannyApp/app/src/main/java/com/example/dannyappPokemonApp/Util/Constants.java
@@ -3,8 +3,7 @@ package com.example.dannyappPokemonApp.Util;
 public class Constants {
 
     public static final String Base_URL = "https://api.pokemontcg.io";
-            // public static final String API
-    public static final String API_KEY= "";
+    public static final String API_KEY= "625414f8-8608-4a99-ba44-993c98799c8a";
     public static final int CONNECTION_TIMEOUT = 3000;
     public static final int READ_TIMEOUT = 2;
     public static final int WRITE_TIMEOUT = 2;
